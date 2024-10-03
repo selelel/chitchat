196607c8164f4458ce8ea1135469757192112f57
Jan Russel Gorembalem <janrusselgorembalem4@gmail.com>
2024-10-03 15:48:32 +0800

    [FIX] refreshing token flow

--- src/lib/features/app/fetchRequestToken.ts
+++ src/lib/features/app/fetchRequestToken.ts
@@ -6,9 +6,19 @@ import { GraphQLClient } from 'graphql-request';
 export const fetchRefreshToken = async () => {
     const client = new GraphQLClient(GRAPHQL_URI, { credentials: 'include' });
     try {
-        const response = await client.request(RefreshTokenQueryDocument) as { refresh: Query['refresh'] };
-        return response?.refresh?.accesstoken;
-    } catch (error) {
-        throw error
+      const response = await client.request(RefreshTokenQueryDocument) as { refresh: Query['refresh'] };
+      return response?.refresh?.accesstoken;
+    } catch (error: unknown) {
+
+        console.log(error, error instanceof Error)
+      if (error instanceof Error) {
+        if (error.message === 'INVALID_REFRESH_TOKEN') {
+            console.log(error, error instanceof Error)
+          window.location.href = '/login';
+        }
+      }
+  
+      throw error;
     }
-};
+  };
+  
